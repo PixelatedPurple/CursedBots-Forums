8827219f235d39a139e646e86b0a9e62cd2b1395
Dep1xel <85250250+PixelatedPurple@users.noreply.github.com>
2023-04-06 12:50:06 -0500

    Create header2.php

---
+++ header2.php
@@ -0,0 +1,13 @@
+<?php
+
+
+echo '<header id="header">
+            <h1><a href="index.php"><img src="/static/header.png" alt="CursedBots | Forums" class="fheader"></a></h1>
+          </header>';
+?>
+<style>
+  .fheader {
+    width: 150px;
+    height: 50px;
+  }
+</style>
